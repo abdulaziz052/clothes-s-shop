195e48a7f722300225c6c9b82d8009dd84d7b046
abdulaziz052 <killerabdulaziz60@gmail.com>
2024-08-16 17:08:36 +0500

    ad

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,6 @@ closeMenu.addEventListener('click' , function(){
 })
 
 
-
 // Responsive menu
 
 
@@ -32,3 +31,14 @@ info_btn.forEach((item  , index)=>{
     })
 })
 
+let activeBox = document.querySelectorAll('#activeBox')
+
+activeBox.forEach((item,index)=>{
+    item.addEventListener('click' , function(){
+        activeBox.forEach((item , index) =>{
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+
+})
